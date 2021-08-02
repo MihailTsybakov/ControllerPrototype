5a0a645aa65d8bb0c92e4f7c8bc7dccf846055b1
MihailTsybakov <mihailtsybakov@yandex.ru>
2021-08-02 15:43:38 +0300

    Inherited exceptions from std::runtime_error

--- exceptions.h
+++ exceptions.h
@@ -4,24 +4,26 @@
 #include <exception>
 #include <string>
 
-class ProcessControllerException : public std::exception
+class ProcessControllerException : public std::runtime_error
 {
 private:
 	std::string error_msg;
 	int error_code;
 public:
-	ProcessControllerException(std::string error_message, int err_code) : error_msg(error_message), error_code(err_code) {}
+	ProcessControllerException(std::string error_message, int err_code) : error_msg(error_message), error_code(err_code),
+																			std::runtime_error(error_message){}
 	int errCode() const noexcept { return error_code; }
 	const char* what() const noexcept override { return error_msg.c_str(); }
 };
 
-class ProcessTaskException : public std::exception
+class ProcessTaskException : public std::runtime_error
 {
 private:
 	std::string error_msg;
 	int error_code;
 public:
-	ProcessTaskException(std::string error_message, int err_code) : error_msg(error_message), error_code(err_code){}
+	ProcessTaskException(std::string error_message, int err_code) : error_msg(error_message), error_code(err_code),
+																		std::runtime_error(error_message) {}
 	int errCode() const noexcept { return error_code; }
 	const char* what() const noexcept override { return error_msg.c_str(); }
 };
