dea477b1814d2d31b8df678c60972f137662ab94
MihailTsybakov <mihailtsybakov@yandex.ru>
2021-08-04 17:11:32 +0300

    Include added

--- main.cpp
+++ main.cpp
@@ -7,17 +7,19 @@
 #include "solvertest.h"
 #include "petsccgtest.h"
 #include "process.h"
+#include <csignal>
 
 int main(int argc, char* argv[])
 {
+
 	auto pc = ProcessController::getInstance();
+	//std::raise(SIGSEGV);
 	pc->initialize();
 
 	std::unique_ptr<SolverTest> test4k1 = std::make_unique<PETScCGTest>();
 	test4k1->A_name = "TestHomoStress4k.mtx";
 	test4k1->file_format = MatrixFileFormat::MTX;
 	test4k1->test();
-
 
 	pc->finalize();
 
